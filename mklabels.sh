a232fa2169fdca2c928a5f7c4e27c8d41d82e240
Alex <eidos92@gmail.com>
2018-01-09 08:32:11 -0700

    Compress all temporal files

--- mklabels.sh
+++ mklabels.sh
@@ -16,23 +16,23 @@ fi
 
 cd $OUTDIR
 
-if [ ! -f all_wmN.nii ]; then
-   3dcalc -a aparc+aseg.nii.gz -expr 'equals(a,2)+equals(a,41)+equals(a,7)+equals(a,16)+equals(a,46)+and(step(a-250),step(256-a))' -prefix all_wmN.nii
+if [ ! -f all_wmN.nii.gz ]; then
+   3dcalc -a aparc+aseg.nii.gz -expr 'equals(a,2)+equals(a,41)+equals(a,7)+equals(a,16)+equals(a,46)+and(step(a-250),step(256-a))' -prefix all_wmN.nii.gz
 fi
 
-if [ ! -f all_gmN.nii ]; then
-   3dcalc -a aparc+aseg.nii -expr 'and(step(a-1000),step(1036-a))+and(step(a-2000),step(2036-a))+and(step(a-7),step(14-a))+and(step(a-16),step(21-a))+and(step(a-25),step(29-a))+and(step(a-46),step(56-a))+and(step(a-57),step(61-a))' -prefix all_gmN.nii
+if [ ! -f all_gmN.nii.gz ]; then
+   3dcalc -a aparc+aseg.nii.gz -expr 'and(step(a-1000),step(1036-a))+and(step(a-2000),step(2036-a))+and(step(a-7),step(14-a))+and(step(a-16),step(21-a))+and(step(a-25),step(29-a))+and(step(a-46),step(56-a))+and(step(a-57),step(61-a))' -prefix all_gmN.nii.gz
 fi
 
-if [ ! -f labels.nii ]; then
-	3dcalc -a all_gmN.nii -b all_wmN.nii -expr 'a+2*b' -prefix labels.nii
+if [ ! -f labels.nii.gz ]; then
+	3dcalc -a all_gmN.nii.gz -b all_wmN.nii.gz -expr 'a+2*b' -prefix labels.nii.gz
 fi
 
 python << END
 from nipy import save_image, load_image
 import numpy as np
 T1 = load_image('${OUTDIR}/T1.nii.gz')
-labels = load_image('${OUTDIR}/labels.nii')
+labels = load_image('${OUTDIR}/labels.nii.gz')
 
 
 np.save('${OUTDIR}/affine.npy',T1.affine)
@@ -41,8 +41,8 @@ np.save('${OUTDIR}/labels.npy', labels.get_data())
 
 END
 
-rm -rf ${OUTDIR}/all_wm.nii
-rm -rf ${OUTDIR}/all_gm.nii
-rm -rf ${OURDIR}/labels.nii
+rm -rf ${OUTDIR}/all_wm.nii.gz
+rm -rf ${OUTDIR}/all_gm.nii.gz
+rm -rf ${OURDIR}/labels.nii.gz
 
 cd $CURDIR
